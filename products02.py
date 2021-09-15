7ff45a89362fa8950fcde7b604f7f76cd4bb5dac
fishcat7 <selaphi7@gmail.com>
2021-09-15 21:43:48 +0800

    first commit

--- products02.py
+++ products02.py
@@ -1,38 +1,52 @@
 import os #作業系統模組
 
 #讀取檔案
-products = []
-if os.path.isfile('products.csv'): #檢查檔案是否存在
-	print('yes')
-	with open('products.csv', 'r', encoding='utf-8') as f:
+def read_file(filename):
+	products = []
+	with open(filename, 'r', encoding='utf-8') as f:
 		for line in f:
 			if '商品,價格' in line:
 				continue #繼續
 			name, price = line.strip().split(',')
-			products.append([name, price])  
-	print(products)
-
-else:
-	print('no')
-
-
+			products.append([name, price])
+	return products
 
 #讓使用者輸入
-while True:
-	name = input('請輸入商品名稱: ')
-	if name == 'q':
-		break
-	price = input('請輸入商品價格: ')
-	price = int(price)
-	products.append([name, price])
-print(products)
+def user_input(products):
+	while True:
+		name = input('請輸入商品名稱: ')
+		if name == 'q':
+			break
+		price = input('請輸入商品價格: ')
+		price = int(price)
+		products.append([name, price])
+	print(products)
+	return products
 
 #印出購買紀錄
-for p in products:
-	print(p[0], '的價格是', p[1])
-
-#寫入檔案	
-with open('products.csv', 'w', encoding='utf-8') as f:  
-	f.write('商品,價格\n')
+def print_products(products):
 	for p in products:
-		f.write(p[0] + ',' + str(p[1]) + '\n')
+		print(p[0], '的價格是', p[1])
+
+#寫入檔案
+def write_file(filename, products):	
+	with open(filename, 'w', encoding='utf-8') as f:  
+		f.write('商品,價格\n')
+		for p in products:
+			f.write(p[0] + ',' + str(p[1]) + '\n')
+
+def main():
+	filename = 'products.csv'
+	if os.path.isfile(filename):  #檢查檔案是否存在
+		print('yes')
+		products = read_file(filename)
+	else:
+		products = []
+		print('no')
+
+	products = user_input(products)
+	print_products(products)
+	write_file('products.csv', products)
+
+
+main()
